64a1815062f4294415218726b4cfbf6d4ffa78ef
cedric <cedric>
2008-05-14 20:54:15 +0000

    Fix inlined string dump/undump.

--- src/lib/eet_data.c
+++ src/lib/eet_data.c
@@ -1557,11 +1557,22 @@ _eet_data_dump_encode(Eet_Dictionary *ed,
 	     data = _eet_data_dump_encode(ed, n, &size);
 	     if (data)
 	       {
-		  eet_data_stream_write(ds, data, size);
+		  echnk = eet_data_chunk_new(data, size, node->name, node->type, node->type);
+		  eet_data_chunk_put(ed, echnk, ds);
+		  eet_data_chunk_free(echnk);
 		  free(data);
 	       }
 	  }
-	break;
+
+	/* Hash is somekind of special case, so we should embed it inside another chunk. */
+	*size_ret = ds->pos;
+	cdata = ds->data;
+
+	ds->data = NULL;
+	ds->size = 0;
+	eet_data_stream_free(ds);
+
+	return cdata;
       case EET_T_CHAR:
         data = eet_data_put_type(ed, node->type, &(node->data.c), &size);
 	if (data)
@@ -1643,6 +1654,13 @@ _eet_data_dump_encode(Eet_Dictionary *ed,
 	  }
 	break;
       case EET_T_INLINED_STRING:
+        data = eet_data_put_type(ed, node->type, &(node->data.str), &size);
+	if (data)
+	  {
+	     eet_data_stream_write(ds, data, size);
+	     free(data);
+	  }
+	break;
       case EET_T_STRING:
         data = eet_data_put_type(ed, node->type, &(node->data.str), &size);
 	if (data)
@@ -1857,6 +1875,11 @@ _eet_data_dump_parse(Eet_Dictionary *ed,
 					   else if (!strcmp(tok3, "string:"))
 					     {
 						n->type = EET_T_STRING;
+						n->data.str = strdup(tok4);
+					     }
+					   else if (!strcmp(tok3, "inlined:"))
+					     {
+						n->type = EET_T_INLINED_STRING;
 						n->data.str = strdup(tok4);
 					     }
 					   else
@@ -2284,6 +2307,18 @@ _eet_data_descriptor_decode(const Eet_Dictionary *ed,
 			    snprintf(tbuf, sizeof(tbuf), "%llu", *((unsigned long long *)dd));
 			    dumpfunc(dumpdata, tbuf); break;
 			  case EET_T_INLINED_STRING:
+			      {
+				 char *s;
+
+				 s = *((char **)dd);
+				 if (s)
+				   {
+				      dumpfunc(dumpdata, "inlined: \"");
+				      _eet_data_dump_string_escape(dumpdata, dumpfunc, s);
+				      dumpfunc(dumpdata, "\"");
+				   }
+			      }
+			    break;
 			  case EET_T_STRING:
 			      {
 				 char *s;
